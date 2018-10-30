8741e1dd704fe7b16d1b7c0ef894d36eec8d9031
HunzenVBA <44619392+HunzenVBA@users.noreply.github.com>
2018-10-30 23:51:28 +0100

    Create PublicVars.vb

---
+++ PublicVars.vb
@@ -0,0 +1,14 @@
+Option Explicit
+
+Public time                         As Date
+Public StartTimerVar                As Date
+Public EndTimerVar                  As Date
+Public ws                           As Worksheet
+Public qt                           As QueryTable
+'Public Zeitstempel As String
+Public i                            As Integer
+Public ZeitstempelWS1               As String
+Public ZeitstempelWS2               As String
+Public ZeitstempelWS3               As String
+Public ZeitstempelWS4               As String
+
