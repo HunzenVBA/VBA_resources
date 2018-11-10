3931a808afe8fd035d5683bc6db39e805d16d5aa
HunzenVBA <deniz.kul@tu-dortmund.de>
2018-11-10 16:00:40 +0100

    comments complete intletzteZeile at 124 moved out of for loop need to delete marking all red

--- Abgleichen/Abgleichen.vb
+++ Abgleichen/Abgleichen.vb
@@ -6,47 +6,47 @@ Sub Abgleichen()
     Dim intletzteZeileWS1 As Integer
     Dim intletzteZeileWS2 As Integer
     Dim bolVergeben As Boolean
-    
+
 	'variables WS2
-	Set cStatusWS2 = Worksheets(2).Cells(intZeileWS2, 1).Value
-	Set cCarrierWS2 = Worksheets(2).Cells(intZeileWS2, 2).Value
-	Set cTimeWS2 = Worksheets(2).Cells(intZeileWS2, 3).Value
-	Set cDateWS2 = Worksheets(2).Cells(intZeileWS2, 4).Value
-	Set cDockNoWS2 = Worksheets(2).Cells(intZeileWS2, 5).Value
-	Set cLocationWS2 = Worksheets(2).Cells(intZeileWS2, 7).Value
-	Set cISAWS2 = Worksheets(2).Cells(intZeileWS2, 8).Value
+	Set cStatusWS2 = Worksheets(2).Cells(intZeileWS2, 1 ).Value				'status = Offen, An Tor
+	Set cCarrierWS2 = Worksheets(2).Cells(intZeileWS2, 2 ).Value				'Carriername
+	Set cTimeWS2 = Worksheets(2).Cells(intZeileWS2, 3 ).Value				'Time of Arrival
+	Set cDateWS2 = Worksheets(2).Cells(intZeileWS2, 4 ).Value				'Date of Arrival
+	Set cDockNoWS2 = Worksheets(2).Cells(intZeileWS2, 5 ).Value				'Docknumber
+	Set cLocationWS2 = Worksheets(2).Cells(intZeileWS2, 7 ).Value			'Location = IB Parcel, IB123, PSIBYARD
+	Set cISAWS2 = Worksheets(2).Cells(intZeileWS2, 8 ).Value					'ISA
 
 	'variables WS1
-	Set cOperatorWS1 = Worksheets(1).Cells(intZeileWS1, 3 ).Value
-	Set cOwnerIDWS1 = Worksheets(1).Cells(intZeileWS1, 5 ).Value
-	Set cLocationCodeWS1 = Worksheets(1).Cells(intZeileWS1, 8 ).Value
-	
-	
+	Set cOperatorWS1 = Worksheets(1).Cells(intZeileWS1, 3 ).Value			'Carriername
+	Set cOwnerIDWS1 = Worksheets(1).Cells(intZeileWS1, 5 ).Value			'Docknumber
+	Set cLocationCodeWS1 = Worksheets(1).Cells(intZeileWS1, 8 ).Value		'LocationCode = IB123, PSIBYARD, IB Parcel
+
+
     intletzteZeileWS1 = Worksheets(1).Cells(Rows.Count, 1).End(xlUp).Row
     intletzteZeileWS2 = Worksheets(2).Cells(Rows.Count, 1).End(xlUp).Row
-    For intZeileWS2 = 2 To intletzteZeileWS2
-    Worksheets(2).Cells(intZeileWS2, 1).Interior.Color = vbWhite
+    For intZeileWS2 = 2 To intletzteZeileWS2								'Loop over all written rows in WS2
+    Worksheets(2).Cells(intZeileWS2, 1).Interior.Color = vbWhite			'color all rows of the first Colum in white
     Next intZeileWS2
-    For intZeileWS1 = 2 To intletzteZeileWS1
+    For intZeileWS1 = 2 To intletzteZeileWS1									'loop over all written rows in WS1
         intletzteZeileWS2 = Worksheets(2).Cells(Rows.Count, 1).End(xlUp).Row
-        bolVergeben = False
+        bolVergeben = False														'set Vergeben to FALSE
         For intZeileWS2 = 2 To intletzteZeileWS2                                'loop over rows WS2
-            If cDockNoWS2 = Worksheets(1).Cells(intZeileWS1, 5).Value Then
-                bolVergeben = True
-                cLocationWS2 = Worksheets(1).Cells(intZeileWS1, 8).Value
-                cCarrierWS2 = Worksheets(1).Cells(intZeileWS1, 3).Value
-                cDockNoWS2 = Worksheets(1).Cells(intZeileWS1, 5).Value
-                If cStatusWS2 <> "Empty" Then
-                    cStatusWS2 = Worksheets(1).Cells(intZeileWS1, 12).Value
+            If cDockNoWS2 = cOwnerIDWS1 Then
+                bolVergeben = True												'set Vergeben to TRUE if Docknumber exists in WS2 already
+                cLocationWS2 = cLocationCodeWS1									'write Location in WS2 as LocationCode from WS1
+                cCarrierWS2 = cOperatorWS1										'write Carriername in WS2 as Operator from WS1
+                cDockNoWS2 = cOwnerIDWS1										'write Docknumber in WS2 as LocationCode from WS1 = Silly
+                If cStatusWS2 <> "Empty" Then									'if status in Ws2 is not EMPTY
+                    cStatusWS2 = Worksheets(1).Cells(intZeileWS1, 12).Value		'set status in WS2 as StatusCode from WS1
                 End If
-                If cStatusWS2 = "FULL" Then
+                If cStatusWS2 = "FULL" Then										'if status in WS2 is FULL then set status in WS2 as Offen
                     cStatusWS2 = "Offen"
                 End If
                 'cTimeWS2 = Format(Worksheets(1).Cells(intZeileWS1, 19).Value, "hh:mm")
                 'cDateWS2 = Format(Worksheets(1).Cells(intZeileWS1, 20).Value, "dd.MM.yyyy")
-                Worksheets(2).Cells(intZeileWS2, 8).Value = Worksheets(1).Cells(intZeileWS1, 16).Value
-                Worksheets(2).Cells(intZeileWS2, 13).Value = ""
-                If Weekday(cDateWS2, 2) = 1 Then
+                Worksheets(2).Cells(intZeileWS2, 8).Value = Worksheets(1).Cells(intZeileWS1, 16).Value	'write ISA from WS1
+                Worksheets(2).Cells(intZeileWS2, 13).Value = ""											'Set  Neue Brücke = empty
+                If Weekday(cDateWS2, 2) = 1 Then									'Color the date according to weekday
                     Worksheets(2).Cells(intZeileWS2, 4).Interior.ColorIndex = 46
                 ElseIf Weekday(cDateWS2, 2) = 2 Then
                     Worksheets(2).Cells(intZeileWS2, 4).Interior.ColorIndex = 26
@@ -61,21 +61,21 @@ Sub Abgleichen()
                 Else:
                     Worksheets(2).Cells(intZeileWS2, 4).Interior.Color = vbWhite
                 End If
-                If cDateWS2 = "" Then
+                If cDateWS2 = "" Then												'if Date is empty color in white
                     Worksheets(2).Cells(intZeileWS2, 4).Interior.Color = vbWhite
                 End If
                 If cCarrierWS2 = "MENDEN" _
                 Or cCarrierWS2 = "K&N" Then
-                    Worksheets(2).Cells(intZeileWS2, 2).Interior.Color = vbBlue
+                    Worksheets(2).Cells(intZeileWS2, 2).Interior.Color = vbBlue		'use blue for MENDEN and K&N
                 End If
             End If
-        Next intZeileWS2
-        If bolVergeben = False Then
-                Worksheets(2).Cells(intZeileWS2, 13).Value = "NEW"
-                cLocationWS2 = Worksheets(1).Cells(intZeileWS1, 8).Value
-                cCarrierWS2 = Worksheets(1).Cells(intZeileWS1, 3).Value
-                cDockNoWS2 = Worksheets(1).Cells(intZeileWS1, 5).Value
-                If Worksheets(2).Cells(intZeileWS2, 12).Value = "" Then
+        Next intZeileWS2															'at this point every match of OwnerIDs have been found
+        If bolVergeben = False Then													'following lines are only dealing with new Docknumbers
+                Worksheets(2).Cells(intZeileWS2, 13).Value = "NEW"					'Set Neue Brücke = "NEW"
+                cLocationWS2 = cLocationCodeWS1
+                cCarrierWS2 = cOperatorWS1
+                cDockNoWS2 = cOwnerIDWS1
+                If Worksheets(2).Cells(intZeileWS2, 12).Value = "" Then				'if Comment is empty write status from StatusCode in WS1
                     cStatusWS2 = Worksheets(1).Cells(intZeileWS1, 12).Value
                 End If
                 If cStatusWS2 = "FULL" Then
@@ -86,8 +86,8 @@ Sub Abgleichen()
                 cDateWS2 = Format(Worksheets(1).Cells(intZeileWS1, 20).Value, "dd.MM.yyyy")
                 Worksheets(2).Cells(intZeileWS2, 6).Value = 300
 '                Worksheets(2).Cells(intZeileWS2, 31).FormulaR1C1 = "=VLOOKUP(ROUNDUP(RC[-28]*24,0),R2C24:R24C25,2,TRUE)"
-                
-                
+
+
             If Weekday(cDateWS2, 2) = 1 Then
                 Worksheets(2).Cells(intZeileWS2, 4).Interior.ColorIndex = 46
             ElseIf Weekday(cDateWS2, 2) = 2 Then
@@ -110,21 +110,21 @@ Sub Abgleichen()
             Then Worksheets(2).Cells(intZeileWS2, 2).Interior.Color = vbBlue
         End If
     Next intZeileWS1
-    intletzteZeileWS2 = Worksheets(2).Cells(Rows.Count, 1).End(xlUp).Row
-    For intZeileWS2 = intletzteZeileWS2 To 2 Step -1
+    intletzteZeileWS2 = Worksheets(2).Cells(Rows.Count, 1).End(xlUp).Row							'neubestimmung letzte Zeile WS2
+    For intZeileWS2 = intletzteZeileWS2 To 2 Step -1												'loop over WS2
         bolVergeben = False
-        For intZeileWS1 = 2 To intletzteZeileWS1
-            If Worksheets(2).Cells(intZeileWS2, 8).Value = Worksheets(1).Cells(intZeileWS1, 16).Value _
-            And cDockNoWS2 = Worksheets(1).Cells(intZeileWS1, 5).Value _
-            Then bolVergeben = True
+        For intZeileWS1 = 2 To intletzteZeileWS1														'loop over WS1
+            If Worksheets(2).Cells(intZeileWS2, 8).Value = Worksheets(1).Cells(intZeileWS1, 16).Value _	'if ISA from WS2 matches with ISA fom WS1
+            And cDockNoWS2 = cOwnerIDWS1 Then																'and Docknumber = owner
+				bolVergeben = True
         Next intZeileWS1
-'        If bolVergeben = False _
-'        Then Worksheets(2).Rows(intZeileWS2).Delete
-    Next intZeileWS2
+        If bolVergeben = False Then
+			Worksheets(2).Rows(intZeileWS2).Delete
+    Next intZeileWS2																				'abgeschlossen: in WS2 gibt es keine mit WS1 nicht übereinstimmende ISA
+	intletzteZeileWS2 = Worksheets(2).Cells(Rows.Count, 1).End(xlUp).Row
     For intZeileWS2 = 2 To intletzteZeileWS2
-        intletzteZeileWS2 = Worksheets(2).Cells(Rows.Count, 1).End(xlUp).Row
         If Left(cLocationWS2, 3) = "IB0" _
-        And cStatusWS2 = "Offen" _
+        And cStatusWS2 = "Offen" _																	'falls Location = IBO und Status offen, dann Tornummer in schreiben
             Then cStatusWS2 = "An Tor: " & CStr(CInt(Right(cLocationWS2, 2)))
     Next intZeileWS2
     bolamTor = False
@@ -133,7 +133,7 @@ Sub Abgleichen()
     Worksheets(2).Cells(intZeileWS2, 1).Interior.Color = vbWhite
         If Left(cLocationWS2, 2) = "IB" And bolamTor = False Then
             bolamTor = True
-            intamTor = intZeileWS2
+            intamTor = intZeileWS2																	'intZeile = Anzahl Brücken am Tor = intamTor
         End If
     Next intZeileWS2
     For intZeileWS2 = 2 To intamTor
