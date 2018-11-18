bd5405dde23c542c81b5f0ccf861e50bde279778
HunzenVBA <deniz.kul@tu-dortmund.de>
2018-11-18 19:38:52 +0100

    add

--- Rodeo/PivotCache.vb
+++ Rodeo/PivotCache.vb
@@ -15,7 +15,6 @@ Dim pT As PivotTable
     Debug.Print ThisWorkbook.PivotCaches.Count
     Debug.Print pC.MemoryUsed, pC.RecordCount, pC.Version
 
-
 End Sub
 
 Sub AddPTandPivotCache()
@@ -23,7 +22,6 @@ Sub AddPTandPivotCache()
 Dim ws As Worksheet
 Dim pC As PivotCache
 Dim pT As PivotTable
-
 
     Set pC = ThisWorkbook.PivotCaches.Create( _
     SourceType:=xlDatabase, _
@@ -35,11 +33,8 @@ Dim pT As PivotTable
     TableDestination:=ActiveCell, _
     TableName:= "ImportPivot2")
 
-
-
     Debug.Print ThisWorkbook.PivotCaches.Count
     Debug.Print pC.MemoryUsed, pC.RecordCount, pC.Version
-
 
 End Sub
 
@@ -49,14 +44,14 @@ Dim pC As PivotCache
 Dim pT As PivotTable
 Dim pvtField as PivotField
 
-  If ThisWorkbook.PivotCaches.Count = 0 THen
+  If ThisWorkbook.PivotCaches.Count = 0 Then
   Set pC = ThisWorkbook.PivotCaches.Create( _
   SourceType:=xlDatabase, _
   SourceData:=shtImportSomethingxyz.Name & "!" & shtImportSomethingxyz.Range("A1").CurrentRegion.Address, _
   Version:=xlPivotTableVersion15)
   Else
     Set pC = ThisWorkbook.PivotCaches(1)
-    End If
+  End If
 
     Set pT = pC.CreatePivotTable( _
     TableDestination:=ActiveCell, _
---
+++ Rodeo/Vorgehen.vb
@@ -0,0 +1,11 @@
+0. Rodeo Filter setzen
+  Workpool: Crossdock + (Transship Sorted)
+  Dwell Time: <1h
+
+  - Total Tabelle
+  - filter outer Scannable ID = PID ws + induct
+  - filter outer Container Type = Workstation + Conveyor
+  
+
+1. Rodeo Import
+  - QueryTable add und refresh
