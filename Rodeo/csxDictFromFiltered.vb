51bf5611ff5a14bcd17f6dac1c1c8473faef9a07
HunzenVBA <44619392+HunzenVBA@users.noreply.github.com>
2018-11-22 11:49:27 +0100

    u

--- Rodeo/csxDictFromFiltered.vb
+++ Rodeo/csxDictFromFiltered.vb
@@ -17,20 +17,26 @@ Application.ScreenUpdating = False
     Dim WorkpoolData As Variant
     Dim timeStampscsx As Variant
 
+    Dim arDataset As Variant
+    Dim dicDataset As Variant
+
     Dim lRow As Long
     Dim lCol As Long
+
+    Dim dicRow As Long
 
     Dim csxDict As Dictionary
 
     Set csxDict = New Dictionary
+    csxDict.RemoveAll
 '    'check if files are open
     On Error Resume Next
-    Set ImportWbk = Workbooks(strRodeoHistoryFileName)
+    Set ImportWbk = Workbooks(strRodeoPath & "RodeoOhneLoadedundPalletized.xlsx")
     Set csxWbk = Workbooks(strcsxStampsFileName)
     On Error GoTo 0
 
     If ImportWbk Is Nothing Then
-        Set ImportWbk = Workbooks.Open(FileName:=strRodeoHistoryFile, UpdateLinks:=False)
+        Set ImportWbk = Workbooks.Open(FileName:=strRodeoPath & "RodeoOhneLoadedundPalletized.xlsx", UpdateLinks:=False)
     Else
         ImportWbk.Close SaveChanges:=False
     End If
@@ -40,15 +46,15 @@ Application.ScreenUpdating = False
         csxWbk.Close SaveChanges:=False
     End If
 
-    Set ImportWbk = Workbooks.Open(FileName:=strRodeoHistoryFile, UpdateLinks:=False)
+    Set ImportWbk = Workbooks.Open(FileName:=strRodeoPath & "RodeoOhneLoadedundPalletized.xlsx", UpdateLinks:=False)
     Set csxWbk = Workbooks.Open(FileName:=strcsxStampsFile, UpdateLinks:=False)
 
     For Each importWS In ImportWbk.Worksheets
             timeStampscsx = Right(importWS.Name, 9)
             lastrow = fLastWrittenRow(importWS, 1)
 
-            csxData = importWS.Range("I1:I" & lastrow).Value2
-            OuterScannableData = importWS.Range("J1:J" & lastrow).Value2
+            csxData = importWS.Range("h1:h" & lastrow).Value2
+            OuterScannableData = importWS.Range("i1:i" & lastrow).Value2
             OuterContainerData = importWS.Range("K1:K" & lastrow).Value2
             WorkpoolData = importWS.Range("O1:O" & lastrow).Value2
 
@@ -60,11 +66,19 @@ Application.ScreenUpdating = False
 
             'If csx doesnt exist in range, then add to dict
             For lRow = LBound(csxData, 1) To UBound(csxData, 1)
+
                 If Not csxDict.Exists(csxData(lRow, 1)) Then
+                    arDataset = csxData(lRow, 1)
+
                     Debug.Print csxData(lRow, 1) & "  " & timeStampscsx
                     csxDict.Add csxData(lRow, 1), OuterScannableData(lRow, 1)
+
+                    dicDataset = csxDict(csxData(lRow, 1))
+
+                Else
                 End If
             Next lRow
+
 
             Debug.Print "Dictionary unique csx count: " & csxDict.Count
 
@@ -75,17 +89,13 @@ Application.ScreenUpdating = False
 '            Erase timeStampscsx
     Next importWS
 
-'
-'    For lastrow = 0 To csxDict.Count - 1
-'        csxWbk.Worksheets(1).Range("A" & (lastrow)) = csxDict.Keys(lastrow)
-'        csxWbk.Worksheets(1).Range("B" & (lastrow)) = csxDict.Items(lastrow)
-'    Next lastrow
 
     With csxDict
-        csxWbk.Worksheets(2).Cells(1, 1).Resize(.Count, 1) = Application.Transpose(.Keys)
-        csxWbk.Worksheets(2).Cells(1, 2).Resize(.Count, 1) = Application.Transpose(.Items)
+        csxWbk.Worksheets(3).Cells(2, 1).Resize(.Count, 1) = Application.Transpose(.Keys)
+        csxWbk.Worksheets(3).Cells(2, 2).Resize(.Count, 1) = Application.Transpose(.Items)
     End With
 
+    csxWbk.Worksheets(3).Columns.AutoFit
 
 
 End Sub
