0d3ef65c18de4e7592083f5d9e71dc5eaab4343c
HunzenVBA <44619392+HunzenVBA@users.noreply.github.com>
2018-11-30 14:41:23 +0100

    update repeat of filtered

--- Rodeo/globalMod.vb
+++ Rodeo/globalMod.vb
@@ -30,3 +30,35 @@ Anf:
     End If
 
 End Sub
+
+Sub RepeatImportFiltered()
+
+    Dim lAnzahl As String
+    Dim i As Long
+
+Anf:
+    lAnzahl = InputBox("Wie oft soll das Makro laufen ?", , 3)
+
+    If lAnzahl = "" Then Exit Sub
+
+    'Prüfen ob eine Zahl eingegeben wurde
+    If IsNumeric(lAnzahl) Then
+        For i = 1 To CLng(lAnzahl)
+                StartTimeAll = Timer
+                currProcedureName = "RodeoAddQueryTotal"
+                Debug.Print "============================ Beginn Sub " & currProcedureName & " ============================"
+                Debug.Print Now
+                Call RodeoAddWorkpool
+                'Determine how many seconds code took to run
+                SecondsElapsedAll = Round(Timer - StartTimeAll, 1)
+                'Notify user in seconds
+                Debug.Print "This code ran successfully in " & SecondsElapsedAll & " seconds"
+                Debug.Print "Makro Start Nr.: " & i
+                Application.Wait (10)       '10 seconds delay between queries
+        Next i
+    Else
+    MsgBox "Bitte ein Zahl eingeben !", vbInformation
+    GoTo Anf
+    End If
+
+End Sub
